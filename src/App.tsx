09728242b3491eaafe63ce86d1f269323c52daf7
KSONG-cloud <76906456+KSONG-cloud@users.noreply.github.com>
2026-01-05 12:53:49 +1100

    Created the component one unit of food (without the ability to click into it to reveal more info and ability to delete it)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,88 @@
 import { Header } from './components/header';
+import { FoodUnit } from './components/track_pantry/one_unit_of_food';
+
+
+// Types
+import type { FoodUnitType, FoodUnitWithNameType } from './types/food';
+
+
 
 function App() {
 
     const today = new Date();
+
+    const food_names: Record<number, string> = {
+        1: "Chicken",
+        2: "Egg",
+        3: "Milk",
+        4: "Chocolate"
+    }
+
+    const food: FoodUnitType = {
+        id: 1,
+        food_id: 1,
+        expiry_date: null,
+        bestbefore_date: null,
+        added_date: new Date("2024-09-08"),
+        quantity: 3
+    }
+
+
+    const foodList: FoodUnitType[] = [
+        {
+            id: 1,
+            food_id: 1,
+            expiry_date: null,
+            bestbefore_date: null,
+            added_date: new Date("2024-09-08"),
+            quantity: 3
+        }, 
+        {
+            id: 3,
+            food_id: 2,
+            expiry_date: new Date("2025-10-08"),
+            bestbefore_date: null,
+            added_date: new Date("2024-07-08"),
+            quantity: 5
+        }, 
+        {
+            id: 2,
+            food_id: 3,
+            expiry_date: null,
+            bestbefore_date: new Date("2025-03-08"),
+            added_date: new Date("2024-03-08"),
+            quantity: 6
+        }, 
+        {
+            id: 4,
+            food_id: 4,
+            expiry_date: new Date("2025-12-08"),
+            bestbefore_date: null,
+            added_date: new Date("2024-09-08"),
+            quantity: 10
+        }
+
+    ]
+
+    // Food names Processing
+    const food_withName: FoodUnitWithNameType = {...food, "food_name": food_names[food["food_id"]]};
+
+    const foodList_withName: FoodUnitWithNameType[] = foodList.map(unit => ({
+        ...unit,
+        food_name: food_names[unit["food_id"]],
+    }));
 
     return (
         <>
             <Header
               date={today}
             ></Header>
-            <p>Hello World</p>
+            <FoodUnit
+                food={food_withName}
+            ></FoodUnit>
+            { foodList_withName.map( food => (
+                <FoodUnit food={food} />
+            )) }
         </>
     );
 }
---
+++ src/components/track_pantry/one_unit_of_food.tsx
@@ -0,0 +1,100 @@
+// CSS
+import "../../styles/track_pantry/one_unit_of_food.css"
+
+// Assets
+import PlusIcon from "../../assets/icons/plus.svg";
+import MinusIcon from "../../assets/icons/minus.svg";
+
+
+// Types
+import type { FoodUnitWithNameType } from "../../types/food";
+
+// React stuff
+import { useState } from "react";
+
+interface FoodUnitProps {
+    food: FoodUnitWithNameType
+}
+
+
+
+const FoodUnit = ({ food }: FoodUnitProps) => {
+    const [quantity, setQuantity] = useState<number>(food.quantity);
+
+ 
+
+    const handleChange = async (delta: number) => {
+        if (delta <=0 && quantity <= 0) return;
+        setQuantity(prev => prev + delta);
+
+        // try {
+        // await fetch(`/api/food/${id}`, {
+        //     method: 'PATCH',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({ quantity: quantity + delta }),
+        // });
+        // } catch (err) {
+        // setQuantity(prev => prev - delta); // rollback on error
+        // }
+    }
+
+    const formatDate = (label: string, date: Date) => `${label}: ${date.toLocaleDateString("en-AU", { dateStyle: "short" })}`;
+
+    let dateText: string;
+
+    if (food.expiry_date) {
+        dateText = formatDate("Exp", food.expiry_date);
+    } else if (food.bestbefore_date) {
+        dateText = formatDate("Best Before", food.bestbefore_date);
+    } else {
+        dateText = formatDate("Added On", food.added_date);
+    }
+
+
+    return (
+        <div className="foodUnit">
+            <div className="food-icon">🥕</div>
+            <div>
+                <div className="food-name">{ food.food_name }</div>
+                <div className="food-dateinfo">
+                    { dateText }
+                    {/* { food.expiry_date && "Exp: " + food.expiry_date.toLocaleDateString(
+                        "en-AU",
+                        {
+                            dateStyle: "short",
+                        }    
+                    )}
+                    { food.expiry_date == null && food.bestbefore_date 
+                        && "Best Before: " + food.bestbefore_date.toLocaleDateString(
+                            "en-AU",
+                            {
+                                dateStyle: "short",
+                            }    
+                        )
+                    }
+                    { food.expiry_date == null && food.bestbefore_date == null 
+                        && "Added On: " + food.added_date.toLocaleDateString(
+                            "en-AU",
+                            {
+                                dateStyle: "short",
+                            }    
+                        )
+                    } */}
+                </div>
+            </div>
+            <div className="food-unittoggle">
+                <button className="food-addbutton" onClick={() => handleChange(1)}>
+                    <img src={PlusIcon} alt="Plus"/>
+                </button>
+                <div className="food-quantity">{ quantity }</div>
+                <button className="food-minusbutton">
+                    <img src={MinusIcon} alt="Minus" onClick={() => handleChange(-1)}/>
+                </button>
+            </div>
+        </div>
+    )
+
+}
+
+
+export { FoodUnit };
