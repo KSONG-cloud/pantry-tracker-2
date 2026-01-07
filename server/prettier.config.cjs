6ffaceb799a0d438a7c14345f39a8e9a516329b6
KSONG-cloud <76906456+KSONG-cloud@users.noreply.github.com>
2026-01-07 17:19:12 +1100

    Added a backend with Node.js and Express and connected it to the React frontend with a message.

---
+++ server/prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    semi: true, // add semicolons at the end
+    singleQuote: true, // use single quotes
+    trailingComma: 'es5', // adds trailing commas where valid in ES5
+    printWidth: 80, // max line length
+    tabWidth: 4, // indentation
+    bracketSpacing: true, // spaces in object literals
+    arrowParens: 'always', // always include parens in arrow functions
+};
