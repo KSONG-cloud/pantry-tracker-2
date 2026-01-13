49f29be6945e5dedc6cd173113f11dcb02d37ecd
KSONG-cloud <76906456+KSONG-cloud@users.noreply.github.com>
2026-01-13 14:53:27 +1100

    Add missing changes from commit b732f90bb0df5ac3c34b198b1d2d30dec2ac37ec

---
+++ client/src/components/track_pantry/track_pantry.tsx
@@ -0,0 +1,196 @@
+// Components
+import { AddItemModal } from './add_item_modal';
+import { FoodGroup } from './group_of_food';
+import { FoodItemModal } from './food_item_modal';
+
+// React stuff
+import { useEffect, useMemo, useRef, useState } from 'react';
+
+// Types
+import type { FoodUnitType, FoodGroupType } from '../../types/food';
+
+// Info
+import { food, food_names, foodList } from '../../info';
+
+function TrackPantry() {
+    // Food Items and Groups State
+    const [foodGroups, setFoodGroups] = useState<FoodGroupType>({});
+    const [foodItems, setFoodItems] = useState<FoodUnitType[]>([]);
+    const tempIdCounter = useRef(-1);
+
+    // General State
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    // Food Item Modal State
+    const [itemModalFoodUnit, setItemModalFoodUnit] =
+        useState<FoodUnitType | null>(null);
+
+    // Add Item Modal State
+    const [addItemModalOpen, setAddItemModalOpen] = useState<boolean>(false);
+
+
+    // Fetching Food Items and Groups from Database
+    const fetchFoodItems = async () => {
+        // Fetch food items from backend
+        const res = await fetch('http://localhost:3001/food/1');
+        const resJSON = await res.json();
+        setFoodItems(resJSON);
+        setLoading(false);
+    };
+
+    const fetchFoodGroups = async () => {
+        // Fetch food groups from backend
+        const res = await fetch('http://localhost:3001/food-groups');
+        const resJSON = await res.json();
+        const food_groups: FoodGroupType = resJSON[0]['foodgroups'];
+        setFoodGroups(food_groups);
+    };
+
+    // add Food Item and Group Handlers
+    const addFoodItem = async (foodItem: FoodUnitType) => {
+        // Logic to add food item to pantry
+        // Optimistically update UI
+        setFoodItems((prev) => [...prev, foodItem]);
+
+        // Send GET request to backend to see if food name exists
+
+        // If not, send POST request to backend to add food name
+
+        // Then send GET request to backend to get food ID
+
+        // Change to correct food ID in foodItem
+
+        // Send POST request to backend to add food item
+    };
+    const addFoodGroup = async (groupName: string) => {
+        // Logic to add food group
+
+        // How many groups in foodGroups
+        const newGroupId = Object.keys(foodGroups).length + 1;
+        setFoodGroups((prev) => ({ ...prev, [newGroupId]: groupName }));
+
+        // Send POST request to backend to add food group to account
+    };
+
+    // Change Food Item and Group Handlers
+    const changeFoodItem = async (editedFood: FoodUnitType) => {
+        // Logic to change food item in pantry
+        setFoodItems((prev) =>
+            prev.map((item) =>
+                item.id === editedFood.id ? { ...item, ...editedFood } : item
+            )
+        );
+
+        // Send PUT request to backend to update food item
+    };
+
+    // Debugging useEffect
+    useEffect(() => {
+        // console.log('Food items changed:', foodItems);
+        // console.log('Grouped food items changed:', groupFoodItems);
+    }, [foodItems]);
+
+    const changeFoodGroup = async (editedFoodGroups: FoodGroupType) => {};
+
+    // Delete Food Item and Group Handlers
+    const deleteFoodItem = async (foodId: number) => {
+        setFoodItems(prev => prev.filter(item => item.id !== foodId));
+    };
+
+    const deleteFoodGroup = async (foodGroupId: number) => {};
+
+    // Food Item Modal Handlers
+    const openItemModal = (food: FoodUnitType) => {
+        setItemModalFoodUnit(food);
+    };
+    const closeItemModal = () => {
+        setItemModalFoodUnit(null);
+    };
+
+    const saveItemModal = async (editedFood: FoodUnitType) => {
+        // Logic to save edited food item
+        console.log('Saving edited food item:', editedFood);
+
+        // Optimistically update UI
+        changeFoodItem(editedFood);
+
+        // Send PUT request to backend to update food item
+    };
+
+
+    // Add Item Modal Handlers
+    const openAddItemModal = () => {
+        setAddItemModalOpen(true);
+    }
+    const closeAddItemModal = () => {
+        setAddItemModalOpen(false);
+    }
+
+    // Query for food data from pantry table for user_id (Data is grouped by foodgroup_id in backend)
+    // Removed = false
+    // Remember to join with food table to get food names
+
+    // Query for food group names from account table
+    useEffect(() => {
+        fetchFoodGroups();
+        fetchFoodItems();
+        console.log('Fetched food groups and items from backend.');
+    }, []);
+
+    // Group food items by their foodgroup_id
+    const groupFoodItems = useMemo(() => {
+        const groupedItems: Record<number, FoodUnitType[]> = {};
+        foodItems.forEach((item) => {
+            const groupId = item.foodgroup_id;
+            groupedItems[groupId] = [...(groupedItems[groupId] || []), item];
+        });
+        return groupedItems;
+    }, [foodItems, foodGroups]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>
+            {Object.keys(foodGroups).map((groupId) => {
+                const id = Number(groupId);
+                return (
+                    <FoodGroup
+                        key={id}
+                        name={foodGroups[id]}
+                        list={groupFoodItems[id] || []}
+                        changeFoodItem={changeFoodItem}
+                        onFoodClick={openItemModal}
+                        addFoodItem={addFoodItem}
+                        openAddItemModal={openAddItemModal}
+                    />
+                );
+            })}
+
+            {/* Food Item Modal */}
+            {itemModalFoodUnit && (
+                <FoodItemModal
+                    food={itemModalFoodUnit}
+                    foodGroups={foodGroups}
+                    onSave={saveItemModal}
+                    onDelete={deleteFoodItem}
+                    onClose={closeItemModal}
+                />
+            )}
+
+            {/* Add Item Modal */}
+            { addItemModalOpen && 
+                <AddItemModal
+                    tempId={tempIdCounter.current--}
+                    onClose={closeAddItemModal}
+                    onAddItem={addFoodItem}
+                />
+            }
+            
+        </>
+    );
+}
+
+export { TrackPantry };
