6c9adf0c60e063e41dd90dd05196d42198fb525c
KSONG-cloud <76906456+KSONG-cloud@users.noreply.github.com>
2026-04-07 12:28:37 +1000

    Prettier

--- server/src/auth/auth.cookies.ts
+++ server/src/auth/auth.cookies.ts
@@ -6,12 +6,16 @@ export const cookieOptionsAccess: CookieOptions = {
     httpOnly: true,
     secure: isProduction,
     sameSite: 'lax',
-    maxAge: process.env.ACCESS_TOKEN_EXPIRATION ? parseInt(process.env.ACCESS_TOKEN_EXPIRATION) : 900000, // 15 minutes
+    maxAge: process.env.ACCESS_TOKEN_EXPIRATION
+        ? parseInt(process.env.ACCESS_TOKEN_EXPIRATION)
+        : 900000, // 15 minutes
 };
 
 export const cookieOptionsRefresh: CookieOptions = {
     httpOnly: true,
     secure: isProduction,
     sameSite: 'lax',
-    maxAge: process.env.REFRESH_TOKEN_EXPIRATION ? parseInt(process.env.REFRESH_TOKEN_EXPIRATION) : 604800000, // 7 days
+    maxAge: process.env.REFRESH_TOKEN_EXPIRATION
+        ? parseInt(process.env.REFRESH_TOKEN_EXPIRATION)
+        : 604800000, // 7 days
 };
