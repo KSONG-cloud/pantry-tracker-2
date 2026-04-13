d811cff289adde29d5e971261bb1c2b81d969f38
KSONG-cloud <76906456+KSONG-cloud@users.noreply.github.com>
2026-04-13 22:20:59 +1000

    Adding spinner to loading screen

--- client/src/components/loading/loading.tsx
+++ client/src/components/loading/loading.tsx
@@ -1,6 +1,9 @@
+import '../../styles/loading/loading.css';
+
 const Loading = () => {
     return (
         <div className="loading-container">
+            <div className="spinner"></div>
             <p>Loading...</p>
         </div>
     );
