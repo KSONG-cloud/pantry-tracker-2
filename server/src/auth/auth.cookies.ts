c5db04f2474b98a5883ed53feb672deeb29aa7bf
KSONG-cloud <76906456+KSONG-cloud@users.noreply.github.com>
2026-04-13 22:49:13 +1000

    Allowing cross site cookies

--- server/src/auth/auth.cookies.ts
+++ server/src/auth/auth.cookies.ts
@@ -1,11 +1,11 @@
 import type { CookieOptions } from 'express';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction:boolean = process.env.NODE_ENV === 'production';
 
 export const cookieOptionsAccess: CookieOptions = {
     httpOnly: true,
     secure: isProduction,
-    sameSite: 'lax',
+    sameSite: process.env.COOKIE_SECURITY as boolean | "lax" | "none" | "strict" || "none",
     maxAge: process.env.ACCESS_TOKEN_EXPIRATION
         ? parseInt(process.env.ACCESS_TOKEN_EXPIRATION)
         : 900000, // 15 minutes
